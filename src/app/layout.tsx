4444cf719c3cbb4cfacde98e2dbbf6cff81fd5a9
yugamitsui <mitsui.yuga@gmail.com>
2025-06-17 11:33:01 +0700

    Install react-hot-toast for toast notifications

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
+import { Toaster } from "react-hot-toast";
 
 export const metadata: Metadata = {
   title: "Focuspace",
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body className="min-h-screen text-white/75 bg-black text-base antialiased font-sans tabular-nums">
         <Header />
         {children}
+        <Toaster />
       </body>
     </html>
   );
